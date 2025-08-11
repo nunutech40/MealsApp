b17246db413221bb563f48ee13f416fbc37cf5cc
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2025-08-11 13:48:42 +0700

    meals app first commit - init

---
+++ MealsApp/MealsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsAppApp.swift
+//  MealsApp
+//
+//  Created by Nunu Nugraha on 11/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MealsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
