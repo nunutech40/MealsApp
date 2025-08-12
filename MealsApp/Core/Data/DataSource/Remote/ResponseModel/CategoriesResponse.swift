833dcd4b4fa4307bfba0d52c3af50d9eb04ae8b0
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2025-08-12 21:06:16 +0700

    add api call + category response

---
+++ MealsApp/Core/Data/DataSource/Remote/ResponseModel/CategoriesResponse.swift
@@ -0,0 +1,30 @@
+//
+//  CategoriesResponse.swift
+//  MealsApp
+//
+//  Created by Nunu Nugraha on 12/08/25.
+//
+
+import Foundation
+
+struct CategoriesResponse: Decodable {
+    
+    let categories: [CategoriesResponse]
+    
+}
+
+struct CategoryResponse: Decodable {
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "idCategory"
+        case title = "strCategory"
+        case image = "strCategoryThumb"
+        case description = "strCategoryDescription"
+    }
+    
+    let id: String?
+    let title: String?
+    let image: String?
+    let description: String?
+    
+}
---
+++ MealsApp/Core/Utils/Network/APICall.swift
@@ -0,0 +1,63 @@
+//
+//  APICall.swift
+//  MealsApp
+//
+//  Created by Nunu Nugraha on 12/08/25.
+//
+
+import Foundation
+
+struct API {
+    static let baseURL = "https://www.themealdb.com/api/json/v1/1/"
+}
+
+protocol EndPoint {
+    var url: String { get }
+}
+
+enum EndPoints {
+    enum Gets: EndPoint {
+        case categories
+        case meals
+        case meal
+        case search
+        
+        public var url: String {
+            switch self {
+            case .categories:
+                return "\(API.baseURL)categories.php"
+            case .meals:
+                return "\(API.baseURL)filter.php?c="
+            case .meal:
+                return "\(API.baseURL)lookup.php?i="
+            case .search:
+                return "\(API.baseURL)search.php?s="
+            }
+        }
+    }
+    
+    enum Post: EndPoint {
+        case addFavorite(mealId: String, userId: String)
+        case createMeal(name: String, category: String)
+        
+        public var url: String {
+            switch self {
+            case .addFavorite(let mealddID, let userID):
+                return "\(API.baseURL)addFavorite.php?mealID=\(mealddID)&userID=\(userID)"
+            case .createMeal(let name, let category):
+                return "\(API.baseURL)createMeal.php?name=\(name)&category=\(category)"
+            }
+        }
+    }
+    
+    enum Deletes: EndPoint {
+        case removeFavorites(mealId: String, userId: String)
+        
+        public var url: String {
+            switch self {
+            case .removeFavorites(let mealId, userId: let userId):
+                return "\(API.baseURL)removeFavorite.php?mealID=\(mealId)&userID=\(userId)"
+            }
+        }
+    }
+}
