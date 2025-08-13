826f405cac80c8235be211f4cd849bb74de9ca7c
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2025-08-13 16:09:04 +0700

    add detail view on detailview

--- MealsApp/Feature/MealApp/Detail/View/DetailView.swift
+++ MealsApp/Feature/MealApp/Detail/View/DetailView.swift
@@ -12,6 +12,58 @@ struct DetailView: View {
     @ObservedObject var presenter: DetailPresenter
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if presenter.loadingState {
+                loadingIndicator
+            } else {
+                ScrollView(.vertical) {
+                    VStack {
+                        imageCategory
+                        spacer
+                        content
+                        spacer
+                    }.padding()
+                }
+            }
+        }
+    }
+}
+
+extension DetailView {
+    var spacer: some View {
+        Spacer()
+    }
+    
+    var loadingIndicator: some View {
+        VStack {
+            Text("Loading...")
+            ProgressView()
+        }
+    }
+    
+    var imageCategory: some View {
+        AsyncImage(url: URL(string: self.presenter.category.image)) { image in
+            image.resizable()
+        } placeholder: {
+            ProgressView()
+        }.scaledToFit().frame(width: 250.0, height: 250.0, alignment: .center)
+    }
+    
+    var description: some View {
+        Text(self.presenter.category.description)
+            .font(.system(size: 15))
+    }
+    
+    func headerTitle(_ title: String) -> some View {
+        return Text(title)
+            .font(.headline)
+    }
+    
+    var content: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            headerTitle("Description")
+                .padding([.top, .bottom])
+            description
+        }
     }
 }
