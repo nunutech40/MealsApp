e66420045790caf9a52c6a4210ee60e81833243a
nunutech40 <49187517+nunutech40@users.noreply.github.com>
2025-08-16 13:33:50 +0700

    refactor algoritma and code for getcategories in mealrepository -> with new algoritma

--- MealsApp/Core/Data/Repository/MealRepository.swift
+++ MealsApp/Core/Data/Repository/MealRepository.swift
@@ -15,21 +15,70 @@ protocol MealRepositoryProtocol {
 
 final class MealRepository: NSObject {
     
-    typealias MealInstance = (RemoteDataSource) -> MealRepository
+    typealias MealInstance = (RemoteDataSource, LocalDataSource) -> MealRepository
     
     fileprivate let remote: RemoteDataSource
+    fileprivate let local: LocalDataSource
     
-    private init(remote: RemoteDataSource) {
+    private init(remote: RemoteDataSource, local: LocalDataSource) {
         self.remote = remote
+        self.local = local
     }
     
-    static let sharedInstance: MealInstance = { remoteRepo in
-        return MealRepository(remote: remoteRepo)
+    static let sharedInstance: MealInstance = { remoteRepo, localRepo in
+        return MealRepository(remote: remoteRepo, local: localRepo)
     }
 }
 
 extension MealRepository: MealRepositoryProtocol {
     func getCategories(result: @escaping (Result<[CategoryModel], any Error>) -> Void) {
+        
+        // get categories dari localdb
+        self.local.getCategories { localResponse in
+            switch localResponse {
+            case.success(let categoryEntity):
+                let categoryList = CategoryMapper.mapCategoryEntityToDomains(input: categoryEntity)
+                // cek jika kosong, ambil data dari remote jika tidak kosong result teruskan ke success (masuk ke usecase)
+                if categoryList.isEmpty {
+                    // ambil dari remote
+                    self.remote.getCategories { remoteResult in
+                        switch remoteResult {
+                        case .success(let categoryResponses):
+                            let categoryEntity = CategoryMapper.mapCategoryResponseToEntities(input: categoryResponses)
+                            // masukka ke local db
+                            self.local.addCategories(from: categoryEntity) { addState in
+                                switch addState {
+                                case .success(let resultFromAdd):
+                                    if resultFromAdd {
+                                        // ambil data dari local db dan teruskan ke success utk di pake di usecase
+                                        self.local.getCategories { localResponse in
+                                            switch localResponse {
+                                            case .success(let categoryEntity):
+                                                let categoryList = CategoryMapper.mapCategoryEntityToDomains(input: categoryEntity)
+                                                result(.success(categoryList))
+                                            case .failure(let error):
+                                                result(.failure(error))
+                                            }
+                                        }
+                                    }
+                                case .failure(let error):
+                                    result(.failure(error))
+                                }
+                                
+                            }
+                        case .failure(let error):
+                            result(.failure(error))
+                        }
+                    }
+                } else { // ini kondisi awal ambil dari remote dan ada datanya, maka langsung teruskan ke success -> di pake di usecase
+                    result(.success(categoryList))
+                }
+                
+            case.failure(let error):
+                result(.failure(error))
+            }
+        }
+        
         self.remote.getCategories { remoteResponse in
             switch remoteResponse {
             case .success(let categoryResponse):
